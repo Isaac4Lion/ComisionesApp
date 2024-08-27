f4451fe7c66cf6fdd3cda0b3d37120ac86f71ae8
Isaac4Lion <92281039+Isaac4Lion@users.noreply.github.com>
2024-08-27 09:11:09 -0500

    #

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -9,10 +9,16 @@ const verificarAutenticacion = async (req,res, next) => {
         const {id, rol} = jwt.verify(authorization.split(' ')[1], process.env.JWT_SECRET)
         if (rol === 'admin'){
             req.admin = await Administradores.findById(id).lean().select('-password')
+            if (!req.admin) {
+                return res.status(404).json({ msg: "Credenciales incorrectas." });
+            }
             req.admin.rol = rol
             next()
         }else if(rol === 'usuario'){
             req.user = await Usuarios.findById(id).lean().select('-password -createdAt -confirmarEmail')
+            if (!req.user) {
+                return res.status(404).json({ msg: "Credenciales incorrectas." });
+            }
             req.user.rol = rol
             next()
         }
